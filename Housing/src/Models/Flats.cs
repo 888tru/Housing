07c175db5471def1766639dbe64c760b23ee7431
888tru <zhanbolatadil000@gmail.com>
2026-06-24 17:15:28 +0500

    v 02 dont handle with both of housing types

--- Housing/src/Models/Flats.cs
+++ Housing/src/Models/Flats.cs
@@ -4,4 +4,12 @@
     public string ResCompName { get; set; }
     public ResComplxType ResComplxType { get; set; }
 
+    public Flats(HousingType type, string country, string city, string region, int price, int rooms, float area, int floors, string resCompName, ResComplxType resComplxType)
+        : base(type, country, city, region, price, rooms, area)
+    {
+        Floors = floors;
+        ResCompName = resCompName;
+        ResComplxType = resComplxType;
+    }
+
 }
